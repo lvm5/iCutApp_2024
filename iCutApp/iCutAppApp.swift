cee2af13d546cb0af9f4366f6febd069a09d7302
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2023-11-04 18:16:05 -0300

    Initial Commit

---
+++ iCutApp/iCutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCutAppApp.swift
+//  iCutApp
+//
+//  Created by Leandro Morais on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iCutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
