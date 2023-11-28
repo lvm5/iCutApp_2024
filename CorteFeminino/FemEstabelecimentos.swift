78c01d7fdd5b6a4163aa8ba5174c50807065a246
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2023-11-28 19:50:40 -0300

    reserves

---
+++ CorteFeminino/FemEstabelecimentos.swift
@@ -0,0 +1,18 @@
+//
+//  FemEstabelecimentos.swift
+//  iCutApp
+//
+//  Created by Leandro Morais on 18/11/23.
+//
+
+import SwiftUI
+
+struct FemEstabelecimentos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FemEstabelecimentos()
+}
---
+++ CorteMasculino/MascEstabelecimentos.swift
@@ -0,0 +1,18 @@
+//
+//  MascEstabelecimentos.swift
+//  iCutApp
+//
+//  Created by Leandro Morais on 18/11/23.
+//
+
+import SwiftUI
+
+struct MascEstabelecimentos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MascEstabelecimentos()
+}
