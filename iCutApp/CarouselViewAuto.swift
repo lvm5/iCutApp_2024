d7f1fc9c9271564fb69ce3b5107fdcdadc8c0ac5
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2024-04-07 18:01:37 -0300

    ads page

---
+++ iCutApp/CarouselViewAuto.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselViewAuto.swift
+//  iCutApp
+//
+//  Created by Leandro Morais on 07/04/24.
+//
+
+import SwiftUI
+
+struct CarouselViewAuto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselViewAuto()
+}
---
+++ iCutApp/CarouselViewChild.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselViewChild.swift
+//  iCutApp
+//
+//  Created by Leandro Morais on 07/04/24.
+//
+
+import SwiftUI
+
+struct CarouselViewChild: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselViewChild()
+}
