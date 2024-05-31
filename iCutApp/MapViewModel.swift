606885d211cdc093c8b95db79ca3cf344323b962
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2024-05-31 19:33:33 -0300

    Chage to ScrollView

---
+++ iCutApp/MapViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewModel.swift
+//  iCutApp
+//
+//  Created by Leandro Morais on 31.05.2024.
+//
+
+import SwiftUI
+
+struct MapViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapViewModel()
+}
