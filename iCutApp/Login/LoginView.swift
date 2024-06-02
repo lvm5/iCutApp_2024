b302e47bb2f45708f4b7d6515e0bb29af404f0ce
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2024-06-02 14:59:39 -0300

    toolBar

---
+++ iCutApp/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  iCutApp
+//
+//  Created by Leandro Morais on 02.06.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
